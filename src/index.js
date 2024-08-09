3a3da8305d75455108d053a6d2c21288d4a8cbd5
josellanos95 <jose.llanos95@gmail.com>
2024-08-08 21:35:59 -0300

    probando vercel deployment

--- src/index.js
+++ src/index.js
@@ -3,5 +3,9 @@ import { connectDB } from "./db.js";
 import { PORT } from "./config/config.js";
 
 connectDB();
-app.listen(PORT);
-console.log(`Server is running on ${PORT}`);
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
+
+export default app;
