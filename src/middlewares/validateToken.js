72d47ce650f824ecbc19f73e929e28f8605127e6
josellanos95 <jose.llanos95@gmail.com>
2024-08-22 20:15:58 -0300

    asd

--- src/middlewares/validateToken.js
+++ src/middlewares/validateToken.js
@@ -2,19 +2,15 @@ import jwt from "jsonwebtoken";
 import { TOKEN_SECRET } from "../config/config.js";
 
 export const authRequired = (req, res, next) => {
-  const authHeader = req.headers['authorization'];
-  const token = authHeader && authHeader.split(' ')[1];
-  
+  const token = req.headers["authorization"]?.split(" ")[1];
   if (!token) {
     return res.status(401).json({ message: "No token, authorization denied" });
   }
-
   try {
     const decoded = jwt.verify(token, TOKEN_SECRET);
     req.user = decoded;
     next();
   } catch (error) {
-    console.error("Error verifying token:", error);
     res.status(401).json({ message: "Token is not valid" });
   }
 };
