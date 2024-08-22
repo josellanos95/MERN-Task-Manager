1befa2d8e8ac85a843d81166d7b510706f80a7c0
josellanos95 <jose.llanos95@gmail.com>
2024-08-22 20:04:18 -0300

    trying to fix task token

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -85,6 +85,7 @@ export const profile = async (req, res) => {
 
 export const verifyToken = async (req, res) => {
   const token = req.headers["authorization"]?.split(" ")[1];
+  console.log(token)
   if (!token) return res.status(401).json({ message: "Token no encontrado" });
 
   jwt.verify(token, TOKEN_SECRET, async (err, user) => {
--- src/middlewares/validateToken.js
+++ src/middlewares/validateToken.js
@@ -2,15 +2,19 @@ import jwt from "jsonwebtoken";
 import { TOKEN_SECRET } from "../config/config.js";
 
 export const authRequired = (req, res, next) => {
-  const token = req.headers["authorization"]?.split(" ")[1];
+  const authHeader = req.headers['authorization'];
+  const token = authHeader && authHeader.split(' ')[1];
+  
   if (!token) {
     return res.status(401).json({ message: "No token, authorization denied" });
   }
+
   try {
     const decoded = jwt.verify(token, TOKEN_SECRET);
     req.user = decoded;
     next();
   } catch (error) {
+    console.error("Error verifying token:", error);
     res.status(401).json({ message: "Token is not valid" });
   }
 };
