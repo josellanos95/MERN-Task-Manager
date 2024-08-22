f33b9c9b8e73d0c1109bd529f715c9de599e7c1e
josellanos95 <jose.llanos95@gmail.com>
2024-08-22 13:32:53 -0300

    deploy

--- src/app.js
+++ src/app.js
@@ -7,7 +7,7 @@ import cors from "cors";
 
 const app = express();
 app.use(cors({
-    origin: "http://localhost:3001",
+    origin: ["http://localhost:3001", "https://mern-task-manager-three.vercel.app"],
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization']
