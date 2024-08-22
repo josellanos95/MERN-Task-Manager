77591d6728024f7c7c3fd18e9a244c7a4e562aaa
josellanos95 <jose.llanos95@gmail.com>
2024-08-22 14:22:03 -0300

    token fixs

--- src/middlewares/validateToken.js
+++ src/middlewares/validateToken.js
@@ -1,13 +1,16 @@
 import jwt from "jsonwebtoken";
-import { TOKEN_SECRET } from "../config/config.js";
 
 export const authRequired = (req, res, next) => {
-  const token = req.cookies.token;
-  if (!token) return res.status(401).json({ message: "No token, authorization denied" });
+  const token = req.cookies.token || req.headers["authorization"];
+  if (!token) {
+    return res.status(401).json({ message: "No token, authorization denied" });
+  }
 
-  jwt.verify(token, TOKEN_SECRET, (err, user) => {
-    if (err) return res.status(401).json({ message: "Token is not valid" });
-    req.user = user;
+  try {
+    const decoded = jwt.verify(token, process.env.TOKEN_SECRET);
+    req.user = decoded.user;
     next();
-  });
+  } catch (error) {
+    res.status(401).json({ message: "Token is not valid" });
+  }
 };
