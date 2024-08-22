dad8af681af1603468e8e917dd6bf30fac891b55
josellanos95 <jose.llanos95@gmail.com>
2024-08-22 14:09:04 -0300

    axios fixed

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,8 +1,7 @@
 import axios from 'axios';
-import { process } from 'node';
 
 const instance = axios.create({
-  baseURL: process.env.VITE_API_URL,
+  baseURL: import.meta.env.VITE_API_URL,
   withCredentials: true, // Asegúrate de incluir las credenciales
 });
 
