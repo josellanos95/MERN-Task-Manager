cd8fa589493d9e3651828c24436e160024598e66
josellanos95 <jose.llanos95@gmail.com>
2024-08-22 18:41:52 -0300

    verificando problema de token

--- src/middlewares/validateToken.js
+++ src/middlewares/validateToken.js
@@ -1,11 +1,17 @@
 import jwt from "jsonwebtoken";
 import { TOKEN_SECRET } from "../config/config.js";
+import { isTokenExpired } from "../libs/tokenUtils.js";
 
 export const authRequired = (req, res, next) => {
-  const token = req.cookies.token || req.headers["authorization"]?.split(" ")[1]; // Asegúrate de obtener el token correctamente
+  const token = req.cookies.token || req.headers["authorization"]?.split(" ")[1];
   if (!token) {
     return res.status(401).json({ message: "No token, authorization denied" });
   }
+
+  if (isTokenExpired(token)) {
+    return res.status(401).json({ message: "Token has expired" });
+  }
+
   try {
     const decoded = jwt.verify(token, TOKEN_SECRET);
     req.user = decoded;
